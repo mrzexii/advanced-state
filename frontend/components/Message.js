9abd2381d6e7149984138053a819696c29812680
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-28 15:39:54 -0800

    Update Message.js

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { connect } from 'react-redux'
+// Message.js
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/action-creators';
 
 function Message(props) {
-  return <div id="message">Nice job!</div>
+  return <div id="message">Nice job!</div>;
 }
 
-export default connect(st => st, actionCreators)(Message)
+export default connect((state) => state, actionCreators)(Message);
