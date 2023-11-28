d24938c8a02aef7786c51e457d645dfb0b7876af
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-28 15:40:45 -0800

    Update action-creators.js

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,70 +1,51 @@
-// ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
-
-export function moveCounterClockwise() { }
-
-export function selectAnswer() { }
-
-export function setMessage() { }
-
-export function setQuiz() { }
-
-export function inputChange() { }
-
-export function resetForm() { }
-
+// action-creators.js
 import axios from 'axios';
 
-export function fetchQuiz() {
+// Synchronous action creators
+export const moveClockwise = () => ({ type: 'MOVE_CLOCKWISE' });
+export const moveCounterClockwise = () => ({ type: 'MOVE_COUNTERCLOCKWISE' });
+export const setQuizIntoState = (quiz) => ({ type: 'SET_QUIZ_INTO_STATE', payload: quiz });
+export const setSelectedAnswer = (answer) => ({ type: 'SET_SELECTED_ANSWER', payload: answer });
+export const setInfoMessage = (message) => ({ type: 'SET_INFO_MESSAGE', payload: message });
+export const inputChange = (value) => ({ type: 'INPUT_CHANGE', payload: value });
+export const resetForm = () => ({ type: 'RESET_FORM' });
+
+// Asynchronous action creators
+export const fetchQuiz = () => {
   return async function (dispatch) {
-    // Dispatch an action to reset the quiz state
     dispatch({ type: 'RESET_QUIZ_STATE' });
 
     try {
-      // Make a GET request to fetch the next quiz
       const response = await axios.get('http://localhost:9000/api/quiz/next');
-
-      // Dispatch an action to send the obtained quiz to its state
       dispatch({ type: 'FETCH_QUIZ_SUCCESS', payload: response.data });
     } catch (error) {
-      // On error, dispatch an action to handle the error
       dispatch({ type: 'FETCH_QUIZ_ERROR', payload: error.message });
     }
   };
-}
+};
 
-export function postAnswer(answerData) {
+export const postAnswer = (answerData) => {
   return async function (dispatch) {
     try {
-      // Make a POST request to submit the answer
       await axios.post('http://localhost:9000/api/quiz/answer', answerData);
-
-      // Dispatch actions for a successful POST
       dispatch({ type: 'RESET_SELECTED_ANSWER_STATE' });
       dispatch({ type: 'SET_SERVER_MESSAGE', payload: 'Answer submitted successfully' });
-
-      // Dispatch the fetching of the next quiz
       dispatch(fetchQuiz());
     } catch (error) {
-      // On error, dispatch an action to handle the error
       dispatch({ type: 'POST_ANSWER_ERROR', payload: error.message });
     }
   };
-}
+};
 
-export function postQuiz(quizData) {
+export const postQuiz = (quizData) => {
   return async function (dispatch) {
     try {
-      // Make a POST request to create a new quiz
       await axios.post('http://localhost:9000/api/quiz/new', quizData);
-
-      // Dispatch actions for a successful POST
       dispatch({ type: 'SET_CORRECT_MESSAGE', payload: 'Quiz created successfully' });
       dispatch({ type: 'RESET_FORM_STATE' });
     } catch (error) {
-      // On error, dispatch an action to handle the error
       dispatch({ type: 'POST_QUIZ_ERROR', payload: error.message });
     }
   };
-}
+};
 
