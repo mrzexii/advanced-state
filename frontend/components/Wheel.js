72c622cbf4adf66b4d4f19f32efe17f3851a0c89
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-28 15:16:04 -0800

    Update Wheel.js

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as actionCreators from '../state/action-creators';
+import React from 'react'
+import { connect } from 'react-redux'
 
 function Wheel(props) {
   return (
     <div id="wrapper">
       <div id="wheel">
-        {/* ... (previous JSX) */}
+        <div className="cog active" style={{ "--i": 0 }}>B</div>
+        <div className="cog" style={{ "--i": 1 }}></div>
+        <div className="cog" style={{ "--i": 2 }}></div>
+        <div className="cog" style={{ "--i": 3 }}></div>
+        <div className="cog" style={{ "--i": 4 }}></div>
+        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" onClick={props.moveCounterClockwise}>
-          Counter clockwise
-        </button>
-        <button id="clockwiseBtn" onClick={props.moveClockwise}>
-          Clockwise
-        </button>
+        <button id="counterClockwiseBtn" >Counter clockwise</button>
+        <button id="clockwiseBtn">Clockwise</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default connect(null, actionCreators)(Wheel);
+export default connect(st => st, actionCreators)(Wheel)
