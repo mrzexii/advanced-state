2d171e616af2af94e1b8e140f1e3ff609d9ec94e
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-28 15:41:02 -0800

    Update reducer.js

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,33 +1,63 @@
-// ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+// reducer.js
+const initialState = {
+  // Your initial state properties go here
+};
 
-const initialWheelState = 0
-function wheel(state = initialWheelState, action) {
-  return state
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Add your case statements for different action types
+    case 'MOVE_CLOCKWISE':
+      // Handle MOVE_CLOCKWISE action
+      return state;
+    case 'MOVE_COUNTERCLOCKWISE':
+      // Handle MOVE_COUNTERCLOCKWISE action
+      return state;
+    case 'SET_QUIZ_INTO_STATE':
+      // Handle SET_QUIZ_INTO_STATE action
+      return state;
+    case 'SET_SELECTED_ANSWER':
+      // Handle SET_SELECTED_ANSWER action
+      return state;
+    case 'SET_INFO_MESSAGE':
+      // Handle SET_INFO_MESSAGE action
+      return state;
+    case 'INPUT_CHANGE':
+      // Handle INPUT_CHANGE action
+      return state;
+    case 'RESET_FORM':
+      // Handle RESET_FORM action
+      return state;
+    case 'RESET_QUIZ_STATE':
+      // Handle RESET_QUIZ_STATE action
+      return state;
+    case 'FETCH_QUIZ_SUCCESS':
+      // Handle FETCH_QUIZ_SUCCESS action
+      return state;
+    case 'FETCH_QUIZ_ERROR':
+      // Handle FETCH_QUIZ_ERROR action
+      return state;
+    case 'RESET_SELECTED_ANSWER_STATE':
+      // Handle RESET_SELECTED_ANSWER_STATE action
+      return state;
+    case 'SET_SERVER_MESSAGE':
+      // Handle SET_SERVER_MESSAGE action
+      return state;
+    case 'POST_ANSWER_ERROR':
+      // Handle POST_ANSWER_ERROR action
+      return state;
+    case 'SET_CORRECT_MESSAGE':
+      // Handle SET_CORRECT_MESSAGE action
+      return state;
+    case 'RESET_FORM_STATE':
+      // Handle RESET_FORM_STATE action
+      return state;
+    case 'POST_QUIZ_ERROR':
+      // Handle POST_QUIZ_ERROR action
+      return state;
+    default:
+      return state;
+  }
+};
 
-const initialQuizState = null
-function quiz(state = initialQuizState, action) {
-  return state
-}
+export default reducer;
 
-const initialSelectedAnswerState = null
-function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
-}
-
-const initialMessageState = ''
-function infoMessage(state = initialMessageState, action) {
-  return state
-}
-
-const initialFormState = {
-  newQuestion: '',
-  newTrueAnswer: '',
-  newFalseAnswer: '',
-}
-function form(state = initialFormState, action) {
-  return state
-}
-
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
