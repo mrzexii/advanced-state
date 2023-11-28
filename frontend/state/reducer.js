bdcdf591758c8d172050eb471f6be2e083312109
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-28 15:47:19 -0800

    Update reducer.js

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,63 +1,70 @@
 // reducer.js
-const initialState = {
-  // Your initial state properties go here
+import { combineReducers } from 'redux';
+
+const initialWheelState = 0;
+const initialQuizState = null;
+const initialSelectedAnswerState = null;
+const initialMessageState = '';
+const initialFormState = {
+  newQuestion: '',
+  newTrueAnswer: '',
+  newFalseAnswer: '',
 };
 
-const reducer = (state = initialState, action) => {
+function wheel(state = initialWheelState, action) {
   switch (action.type) {
-    // Add your case statements for different action types
     case 'MOVE_CLOCKWISE':
-      // Handle MOVE_CLOCKWISE action
-      return state;
+      return state; // Replace with your logic
     case 'MOVE_COUNTERCLOCKWISE':
-      // Handle MOVE_COUNTERCLOCKWISE action
-      return state;
-    case 'SET_QUIZ_INTO_STATE':
-      // Handle SET_QUIZ_INTO_STATE action
-      return state;
-    case 'SET_SELECTED_ANSWER':
-      // Handle SET_SELECTED_ANSWER action
-      return state;
-    case 'SET_INFO_MESSAGE':
-      // Handle SET_INFO_MESSAGE action
-      return state;
-    case 'INPUT_CHANGE':
-      // Handle INPUT_CHANGE action
-      return state;
-    case 'RESET_FORM':
-      // Handle RESET_FORM action
-      return state;
-    case 'RESET_QUIZ_STATE':
-      // Handle RESET_QUIZ_STATE action
-      return state;
-    case 'FETCH_QUIZ_SUCCESS':
-      // Handle FETCH_QUIZ_SUCCESS action
-      return state;
-    case 'FETCH_QUIZ_ERROR':
-      // Handle FETCH_QUIZ_ERROR action
-      return state;
-    case 'RESET_SELECTED_ANSWER_STATE':
-      // Handle RESET_SELECTED_ANSWER_STATE action
-      return state;
-    case 'SET_SERVER_MESSAGE':
-      // Handle SET_SERVER_MESSAGE action
-      return state;
-    case 'POST_ANSWER_ERROR':
-      // Handle POST_ANSWER_ERROR action
-      return state;
-    case 'SET_CORRECT_MESSAGE':
-      // Handle SET_CORRECT_MESSAGE action
-      return state;
-    case 'RESET_FORM_STATE':
-      // Handle RESET_FORM_STATE action
-      return state;
-    case 'POST_QUIZ_ERROR':
-      // Handle POST_QUIZ_ERROR action
-      return state;
+      return state; // Replace with your logic
     default:
       return state;
   }
-};
+}
 
-export default reducer;
+function quiz(state = initialQuizState, action) {
+  switch (action.type) {
+    case 'SET_QUIZ_INTO_STATE':
+      return action.payload; // Replace with your logic
+    default:
+      return state;
+  }
+}
+
+function selectedAnswer(state = initialSelectedAnswerState, action) {
+  switch (action.type) {
+    case 'SET_SELECTED_ANSWER':
+      return action.payload; // Replace with your logic
+    default:
+      return state;
+  }
+}
+
+function infoMessage(state = initialMessageState, action) {
+  switch (action.type) {
+    case 'SET_INFO_MESSAGE':
+      return action.payload; // Replace with your logic
+    default:
+      return state;
+  }
+}
+
+function form(state = initialFormState, action) {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      return { ...state, [action.payload.field]: action.payload.value }; // Replace with your logic
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  wheel,
+  quiz,
+  selectedAnswer,
+  infoMessage,
+  form,
+});
+
+export default rootReducer;
 
