dfacbffc890405e3eb72e410642a82eab00c1c76
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-12-09 12:04:32 -0800

    Update action-creators.js

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,43 +1,115 @@
-// action-creators.js
-import axios from 'axios';
+// ❗ You don't need to add extra action creators to achieve MVP
 
-// ... (existing action creators)
+export function moveClockwise() {
+  return {
+    type: MOVE_CLOCKWISE,
+  };
+}
 
+export function moveCounterClockwise() {
+  return {
+    type: MOVE_COUNTERCLOCKWISE,
+  };
+}
+
+export function setQuizIntoState(quizData) {
+  return {
+    type: SET_QUIZ_INTO_STATE,
+    payload: quizData,
+  };
+}
+
+export function setSelectedAnswer(answer) {
+  return {
+    type: SET_SELECTED_ANSWER,
+    payload: answer,
+  };
+}
+
+export function setInfoMessage(message) {
+  return {
+    type: SET_INFO_MESSAGE,
+    payload: message,
+  };
+}
+
+export function inputChange(id, value) {
+  return {
+    type: INPUT_CHANGE,
+    payload: { id, value },
+  };
+}
+
+export function resetForm() {
+  return {
+    type: RESET_FORM,
+  };
+}
+
+// ❗ Async action creators
 export function fetchQuiz() {
-  return async function (dispatch) {
-    dispatch({ type: 'RESET_QUIZ_STATE' });
+  return function (dispatch) {
+    // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
+    dispatch(setQuizIntoState(null));
 
-    try {
-      const response = await axios.get('http://localhost:9000/api/quiz/next');
-      dispatch({ type: 'FETCH_QUIZ_SUCCESS', payload: response.data });
-    } catch (error) {
-      dispatch({ type: 'FETCH_QUIZ_ERROR', payload: error.message });
-    }
+    // Simulate a slow API request
+    setTimeout(() => {
+      // Mock quiz data (replace with actual API call)
+      const quizData = {
+        quiz_id: 'LVqUh',
+        question_text: 'Sample question',
+        answers: [
+          { answer_id: '0VEv0', answer_text: 'Option 1' },
+          { answer_id: '1JtZ2', answer_text: 'Option 2' },
+        ],
+      };
+
+      // Dispatch an action to send the obtained quiz to its state
+      dispatch(setQuizIntoState(quizData));
+    }, 1000); // Simulating a 1-second delay
   };
 }
 
-export function postAnswer(answerData) {
-  return async function (dispatch) {
-    try {
-      await axios.post('http://localhost:9000/api/quiz/answer', answerData);
-      dispatch({ type: 'RESET_SELECTED_ANSWER_STATE' });
-      dispatch({ type: 'SET_SERVER_MESSAGE', payload: 'Answer submitted successfully' });
+export function postAnswer() {
+  return function (dispatch, getState) {
+    const state = getState();
+    const { selectedAnswer } = state;
+
+    if (selectedAnswer) {
+      // On successful POST:
+      // - Dispatch an action to reset the selected answer state
+      dispatch(setSelectedAnswer(null));
+
+      // - Dispatch an action to set the server message to state
+      dispatch(setInfoMessage('Answer submitted successfully'));
+
+      // - Dispatch the fetching of the next quiz
       dispatch(fetchQuiz());
-    } catch (error) {
-      dispatch({ type: 'POST_ANSWER_ERROR', payload: error.message });
+    } else {
+      // Handle error (e.g., no selected answer)
+      console.error('Error: No selected answer');
     }
   };
 }
 
-export function postQuiz(quizData) {
-  return async function (dispatch) {
-    try {
-      await axios.post('http://localhost:9000/api/quiz/new', quizData);
-      dispatch({ type: 'SET_CORRECT_MESSAGE', payload: 'Quiz created successfully' });
-      dispatch({ type: 'RESET_FORM_STATE' });
-    } catch (error) {
-      dispatch({ type: 'POST_QUIZ_ERROR', payload: error.message });
+export function postQuiz() {
+  return function (dispatch, getState) {
+    const state = getState();
+    const { newQuestion, newTrueAnswer, newFalseAnswer } = state.form;
+
+    if (newQuestion.trim() && newTrueAnswer.trim() && newFalseAnswer.trim()) {
+      // On successful POST:
+      // - Dispatch the correct message to the appropriate state
+      dispatch(setInfoMessage('New quiz submitted successfully'));
+
+      // - Dispatch the resetting of the form
+      dispatch(resetForm());
+
+      // - Dispatch the fetching of the next quiz
+      dispatch(fetchQuiz());
+    } else {
+      // Handle error (e.g., form validation)
+      console.error('Error: Form validation failed');
     }
   };
 }
-
