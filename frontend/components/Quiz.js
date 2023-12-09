2a406433e1e5ae763035ba74335ee081aa7c8277
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-12-09 11:52:32 -0800

    Update Quiz.js

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,36 +1,37 @@
-// Quiz.js
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../state/action-creators';
 
 function Quiz(props) {
+  const { quiz, selectedAnswer } = props;
+
   return (
     <div id="wrapper">
-      {
-        // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
-        props.quiz ? (
-          <>
-            <h2>{props.quiz.question_text}</h2>
-
-            <div id="quizAnswers">
-              <div className="answer selected">
-                {props.quiz.true_answer_text}
-                <button>SELECTED</button>
-              </div>
-
-              <div className="answer">
-                {props.quiz.false_answer_text}
+      {quiz ? (
+        <>
+          <h2>{quiz.question_text}</h2>
+          <div id="quizAnswers">
+            {quiz.answers.map((answer) => (
+              <div key={answer.answer_id} className={`answer ${selectedAnswer === answer.answer_id ? 'selected' : ''}`}>
+                {answer.answer_text}
                 <button>Select</button>
               </div>
-            </div>
-
-            <button id="submitAnswerBtn">Submit answer</button>
-          </>
-        ) : 'Loading next quiz...'
-      }
+            ))}
+          </div>
+          <button id="submitAnswerBtn" disabled={!selectedAnswer}>
+            Submit answer
+          </button>
+        </>
+      ) : (
+        'Loading next quiz...'
+      )}
     </div>
   );
 }
 
-export default connect((state) => state, actionCreators)(Quiz);
+const mapStateToProps = (state) => ({
+  quiz: state.quiz,
+  selectedAnswer: state.selectedAnswer,
+});
 
+export default connect(mapStateToProps, actionCreators)(Quiz);
