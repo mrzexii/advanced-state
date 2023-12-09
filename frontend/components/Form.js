4bfdfcec2d0db9bcaf87c05fa51e1a63a7371cea
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-12-09 11:53:07 -0800

    Update Form.js

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,26 +1,36 @@
-// Form.js
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../state/action-creators';
 
 function Form(props) {
+  const { newQuestion, newTrueAnswer, newFalseAnswer, inputChange, postQuiz } = props;
+
   const onChange = (evt) => {
-    // Implement your onChange logic here
+    inputChange(evt.target.id, evt.target.value);
   };
 
   const onSubmit = (evt) => {
-    // Implement your onSubmit logic here
+    evt.preventDefault();
+    postQuiz();
   };
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input maxLength={50} onChange={onChange} id="newQuestion" value={newQuestion} placeholder="Enter question" />
+      <input maxLength={50} onChange={onChange} id="newTrueAnswer" value={newTrueAnswer} placeholder="Enter true answer" />
+      <input maxLength={50} onChange={onChange} id="newFalseAnswer" value={newFalseAnswer} placeholder="Enter false answer" />
+      <button id="submitNewQuizBtn" disabled={!newQuestion.trim() || !newTrueAnswer.trim() || !newFalseAnswer.trim()}>
+        Submit new quiz
+      </button>
     </form>
   );
 }
 
-export default connect((state) => state, actionCreators)(Form);
+const mapStateToProps = (state) => ({
+  newQuestion: state.form.newQuestion,
+  newTrueAnswer: state.form.newTrueAnswer,
+  newFalseAnswer: state.form.newFalseAnswer,
+});
+
+export default connect(mapStateToProps, actionCreators)(Form);
