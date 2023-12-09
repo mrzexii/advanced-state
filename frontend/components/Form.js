9bd5e84204587cbbd526aa9ff21a54db8d9f8593
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-12-09 12:02:53 -0800

    Update Form.js

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,36 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as actionCreators from '../state/action-creators';
+import React from 'react'
+import { connect } from 'react-redux'
+import * as actionCreators from '../state/action-creators'
 
-function Form(props) {
-  const { newQuestion, newTrueAnswer, newFalseAnswer, inputChange, postQuiz } = props;
+export function Form(props) {
 
-  const onChange = (evt) => {
-    inputChange(evt.target.id, evt.target.value);
-  };
+  const onChange = evt => {
 
-  const onSubmit = (evt) => {
-    evt.preventDefault();
-    postQuiz();
-  };
+  }
+
+  const onSubmit = evt => {
+
+  }
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" value={newQuestion} placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" value={newTrueAnswer} placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" value={newFalseAnswer} placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn" disabled={!newQuestion.trim() || !newTrueAnswer.trim() || !newFalseAnswer.trim()}>
-        Submit new quiz
-      </button>
+      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
+      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
+      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
+      <button id="submitNewQuizBtn">Submit new quiz</button>
     </form>
-  );
+  )
 }
 
-const mapStateToProps = (state) => ({
-  newQuestion: state.form.newQuestion,
-  newTrueAnswer: state.form.newTrueAnswer,
-  newFalseAnswer: state.form.newFalseAnswer,
-});
-
-export default connect(mapStateToProps, actionCreators)(Form);
+export default connect(st => st, actionCreators)(Form)
