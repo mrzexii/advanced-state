1810cac501d17eeab5e7e8f75fe65d2675d942da
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-12-09 18:08:47 -0800

    Update Quiz.js

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,4 +1,4 @@
-export default function Quiz(props) {
+import React from 'react'
 import { connect } from 'react-redux'
 import { fetchQuiz, selectAnswer, postAnswer } from '../state/action-creators'
 function Quiz(props) {
@@ -11,26 +11,10 @@ function Quiz(props) {
 
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
-        true ? (
         props.isFetching ? (
           <>
-            <h2>What is a closure?</h2>
-
             <h2>{props.quiz.question}</h2>
             <div id="quizAnswers">
-              <div className="answer selected">
-                A function
-                <button>
-                  SELECTED
-                </button>
-              </div>
-
-              <div className="answer">
-                An elephant
-                <button>
-                  Select
-                </button>
-              </div>
               {props.quiz.answers.map((answer, ind) => {
                 return <div key={answer.text} className={props.quiz.answers[ind].answerHighlight ? "answer selected" : "answer"}>{answer.text}
                   <button onClick={() => props.selectAnswer(answer.answer_id)}>{props.quiz.answers[ind].selectValue}</button>
@@ -38,7 +22,6 @@ function Quiz(props) {
               })}
             </div>
 
-            <button id="submitAnswerBtn">Submit answer</button>
             <button id="submitAnswerBtn" disabled={props.buttonState} onClick={() => props.postAnswer(props.quiz)}>Submit answer</button>
           </>
         ) : 'Loading next quiz...'
